10a72fbab8ebef45881f3cfab0ec1398581c2b69
elsueter <34282243+elsueter@users.noreply.github.com>
2019-01-19 23:49:52 +0000

    Change Main
    Basically the same, mostly from the same example the bot was running on before

--- main.go
+++ main.go
@@ -29,23 +29,27 @@ func main() {
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("error creating Discord session,", err)
+		fmt.Println("Error creating Discord session: ", err)
 		return
 	}
 
-	// Register the messageCreate func as a callback for MessageCreate events.
-	dg.AddHandler(messageCreate)
-	dg.AddHandler(onGuild)
+	// Register ready as a callback for the ready events.
+	dg.AddHandler(ready)
 
-	// Open a websocket connection to Discord and begin listening.
+	// Register messageCreate as a callback for the messageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Register guildCreate as a callback for the guildCreate events.
+	dg.AddHandler(guildCreate)
+
+	// Open the websocket and begin listening.
 	err = dg.Open()
 	if err != nil {
-		fmt.Println("error opening connection,", err)
-		return
+		fmt.Println("Error opening Discord session: ", err)
 	}
 
 	// Wait here until CTRL-C or other term signal is received.
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	fmt.Println("running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
