e32e786dba2dc1fc4b41903481f725341d863667
Alex W <alexwh@users.noreply.github.com>
2019-01-20 20:33:37 +0000

    add SetupConfig function to config.go

--- config.go
+++ config.go
@@ -1,3 +1,21 @@
 package main
 
-import "github.com/spf13/viper"
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+func SetupConfig() *viper.Viper {
+	config := viper.New()
+	config.SetConfigType("toml")
+	config.SetConfigName("config")
+	config.AddConfigPath("/etc/paul-bot")
+	config.SetDefault("DiscordKey", "")
+
+	err := config.ReadInConfig()
+	if err != nil {
+		log.Panicf("error reading config file: %v", err)
+	}
+	return config
+}
