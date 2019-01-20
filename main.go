e0741162d44026263b98e3c679ec66a339d80669
Alex W <alexwh@users.noreply.github.com>
2019-01-20 03:34:31 +0000

    comment and style fixes

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 	}
 	Token := strings.TrimSuffix(string(dat), "\n")
 
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("Error creating Discord session: ", err)
@@ -36,40 +35,28 @@ func main() {
 		router.FindAndExecute(dg, "!", dg.State.User.ID, m.Message)
 	})
 
-	// Register ready as a callback for the ready events.
 	dg.AddHandler(ready)
-
-	// Register guildCreate as a callback for the guildCreate events.
 	dg.AddHandler(guildCreate)
 
-	// Open the websocket and begin listening.
+	// Open the websocket and begin listening for events.
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("Error opening Discord session: ", err)
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
 
-	// Cleanly close down the Discord session.
 	dg.Close()
 }
 
-// This function will be called (due to AddHandler above) when the bot receives
-// the "ready" event from Discord.
 func ready(s *discordgo.Session, event *discordgo.Ready) {
-
-	// Set the playing status.
 	s.UpdateStatus(0, "Botting It Up")
 }
 
-// This function will be called (due to AddHandler above) every time a new
-// guild is joined.
 func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
-
 	if event.Guild.Unavailable {
 		return
 	}
--- media.go
+++ media.go
@@ -21,17 +21,13 @@ func Play(ctx *exrouter.Context){
 	m := ctx.Msg
 	s := ctx.Ses
 
-	// Find the channel that the message came from.
 	c, err := s.State.Channel(m.ChannelID)
 	if err != nil {
-		// Could not find channel.
 		return
 	}
 
-	// Find the guild for that channel.
 	g, err := s.State.Guild(c.GuildID)
 	if err != nil {
-		// Could not find guild.
 		return
 	}
 
@@ -42,7 +38,6 @@ func Play(ctx *exrouter.Context){
 			if err != nil {
 				fmt.Println("Error playing sound:", err)
 			}
-
 			return
 		}
 	}
@@ -68,12 +63,10 @@ func ytSearch(query string, maxResults int64) map[string]string {
 	if err != nil {
 	}
 
-	// Group video, channel, and playlist results in separate lists.
 	videos := make(map[string]string)
 	channels := make(map[string]string)
 	playlists := make(map[string]string)
 
-	// Iterate through each item and add it to the correct list.
 	for _, item := range response.Items {
 		switch item.Id.Kind {
 		case "youtube#video":
@@ -88,7 +81,6 @@ func ytSearch(query string, maxResults int64) map[string]string {
 	return videos
 }
 
-// PlaySound plays the current buffer to the provided channel.
 func playSound(s *discordgo.Session, guildID, channelID string, search string) {
 	videos := ytSearch(search, 1)
 	var vids []string
@@ -97,18 +89,13 @@ func playSound(s *discordgo.Session, guildID, channelID string, search string) {
 		vids = append(vids, id)
 	}
 
-	// Join the provided voice channel.
 	vc, err := s.ChannelVoiceJoin(guildID, channelID, false, true)
 	if err != nil {
 	}
 
-	// Sleep for a specified amount of time before playing the sound
 	time.Sleep(250 * time.Millisecond)
-
-	// Start speaking.
 	vc.Speaking(true)
 
-	// Change these accordingly
 	options := dca.StdEncodeOptions
 	options.RawOutput = true
 	options.Bitrate = 64
@@ -144,12 +131,7 @@ func playSound(s *discordgo.Session, guildID, channelID string, search string) {
 
 	fmt.Println("3")
 
-	// Stop speaking
 	vc.Speaking(false)
-
-	// Sleep for a specificed amount of time before ending.
 	time.Sleep(250 * time.Millisecond)
-
-	// Disconnect from the provided voice channel.
 	vc.Disconnect()
 }
