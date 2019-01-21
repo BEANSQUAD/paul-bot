2802118ef86c7433f2b2adce919692b11ea6aba3
Roo Thorp <thorp.roo@gmail.com>
2019-01-21 23:28:35 +0000

    Add godoc to main.go
    honestly im just doing this to make this goreport thing better

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// Exit disconnects the bot, and exits the bot.
+// The configuration of docker means that the bot is automatically restarted.
 func Exit(ctx *exrouter.Context){
 	err := player.vConn.Speaking(false)
 	if err != nil {
