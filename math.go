7838b673b452b9f539512ab317b136464a087ad8
Alex W <alexwh@users.noreply.github.com>
2019-01-21 03:56:43 +0000

    change After() to slice syntax
    After() returns a string rather than a slice of strings

--- math.go
+++ math.go
@@ -9,7 +9,7 @@ import (
 
 func Add(ctx *exrouter.Context) {
 	total := 0
-	for _, num := range ctx.Args.After(1) {
+	for _, num := range ctx.Args[1:] {
 		n, err := strconv.Atoi(num)
 		if err != nil {
 			log.Printf("error casting string to int: %v", err)
