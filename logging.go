09a7072335b38b8a4ec98edeb63363365af1fc9e
Alex W <alexwh@users.noreply.github.com>
2019-01-23 01:01:12 +0000

    use a logrus hook instead of a writer

--- logging.go
+++ logging.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"io"
-	"os"
+	"fmt"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/lalamove/konfig"
 	"github.com/sirupsen/logrus"
 )
 
-type discordWriter struct {
+type DiscordHook struct {
 	sess *discordgo.Session
 }
 
-func (w *discordWriter) Write(b []byte) (int, error) {
-	n := len(b)
-	w.sess.ChannelMessageSend(konfig.String("LogChannel"), string(b))
-	return n, nil
+func (h *DiscordHook) Fire(e *logrus.Entry) error {
+	msg, err := e.String()
+	if err != nil {
+		return fmt.Errorf("error in coaxing Entry %v to string: %v", e, err)
+	}
+	_, err = h.sess.ChannelMessageSendEmbed(konfig.String("LogChannel"), &discordgo.MessageEmbed{
+		Title:       "Log event",
+		Description: msg,
+	})
+	if err != nil {
+		return fmt.Errorf("error sending log embed: %v", err)
+	}
+	return nil
+}
+
+func (h *DiscordHook) Levels() []logrus.Level {
+	return logrus.AllLevels
 }
 
 func SetupLogger(s *discordgo.Session) {
-	dw := discordWriter{s}
-	multi := io.MultiWriter(os.Stdout, &dw)
-	logrus.SetOutput(multi)
+	logrus.AddHook(&DiscordHook{s})
 }
