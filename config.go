7e9fe59b5e2787679e2ebf8c070d05ec2477776c
Alex W <alexwh@users.noreply.github.com>
2019-01-24 04:25:37 +0000

    fix Split call order

--- config.go
+++ config.go
@@ -80,7 +80,7 @@ func configSet(key string, value string) error {
 }
 
 func checkDefault(defaultMap map[string]string, key string) error {
-	keyTrans := strings.Split(".", strings.ToLower(key))
+	keyTrans := strings.Split(strings.ToLower(key), ".")
 	key = keyTrans[len(keyTrans)-1]
 	validKeys := make([]string, 0, len(defaultMap))
 
